64644dd9e9018ade4187d6730491845cb4a373e0
jupeeze <pokegames1578@gmail.com>
2025-01-15 00:09:24 +0900

    Create Program.cs

---
+++ Code/Program.cs
@@ -0,0 +1,39 @@
+﻿using DxLibDLL;
+
+internal static class Game
+{
+	public static void Run() {
+		Loop();
+	}
+
+	public static void Loop() {
+		while (DX.ProcessMessage() == 0) {
+			DX.ClearDrawScreen();
+
+			Draw();
+
+			DX.ScreenFlip();
+		}
+	}
+
+	public static void Draw() {
+		DX.DrawString(100, 100, "Hello World", DX.GetColor(255, 255, 255));
+	}
+}
+
+internal class Program
+{
+	private static void Main() {
+		Init();
+
+		Game.Run();
+
+		DX.DxLib_End();
+	}
+
+	private static void Init() {
+		DX.ChangeWindowMode(DX.TRUE);
+		DX.DxLib_Init();
+		DX.SetDrawScreen(DX.DX_SCREEN_BACK);
+	}
+}
