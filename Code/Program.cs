0a1bbf724f0d878b2fd32bca810eace3f0455d9c
jupeeze <pokegames1578@gmail.com>
2025-01-16 17:20:39 +0900

    Create Ground.cs

--- Code/Program.cs
+++ Code/Program.cs
@@ -2,27 +2,39 @@
 
 internal static class Game
 {
+	public static readonly int GROUND_SIZE = 32;
+	public static readonly int GROUND_POS = Program.SCREEN_Y - GROUND_SIZE;
+
 	public static void Run() {
+		Load();
 		Loop();
 	}
 
-	public static void Loop() {
+	private static void Load() {
+		Ground.Load();
+	}
+
+	private static void Loop() {
 		while (DX.ProcessMessage() == 0) {
-			DX.ClearDrawScreen();
-
 			Draw();
-
-			DX.ScreenFlip();
 		}
 	}
 
-	public static void Draw() {
-		DX.DrawString(100, 100, "Hello World", DX.GetColor(255, 255, 255));
+	private static void Draw() {
+		DX.ClearDrawScreen();
+
+		Ground.Draw();
+
+		DX.ScreenFlip();
 	}
 }
 
-internal class Program
+internal static class Program
 {
+	public static readonly int SCREEN_X = 640, SCREEN_Y = 480;
+
+	public static string ASSET_PATH = @"..\..\Assets\";
+
 	private static void Main() {
 		Init();
 
@@ -36,4 +48,14 @@ internal class Program
 		DX.DxLib_Init();
 		DX.SetDrawScreen(DX.DX_SCREEN_BACK);
 	}
+
+	public static int[] LoadSprites(string filePath, int divX, int divY, int sizeX, int sizeY) {
+		int spriteCount = divX * divY;
+		int[] sprites = new int[spriteCount];
+		DX.LoadDivGraph(ASSET_PATH + filePath, spriteCount, divX, divY, sizeX, sizeY, sprites);
+		return sprites;
+	}
+
+	public static void DrawEXGraph(int posX, int posY, int sizeX, int sizeY, int GrHandle) =>
+		DX.DrawExtendGraph(posX, posY, posX + sizeX, posY + sizeY, GrHandle, DX.TRUE);
 }
