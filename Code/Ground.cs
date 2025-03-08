521c98502a011acba13daf2c9816760c5bdb7bc0
jumpei okazaki <pokegames1578@gmail.com>
2025-03-08 13:48:44 +0900

    インデントをタブからスペースに

--- Code/Ground.cs
+++ Code/Ground.cs
@@ -1,18 +1,17 @@
 ﻿using DxLibDLL;
 
-internal class Ground
-{
-	private static readonly int TILE_COUNT = Program.SCREEN_X / Game.GROUND_SIZE + 1;
+internal class Ground {
+    private static readonly int TILE_COUNT = Program.SCREEN_X / Game.GROUND_SIZE + 1;
 
-	private static int[] _groundImages;
+    private static int[] _groundImages;
 
-	public static void Load() {
-		_groundImages = Program.LoadSprites(@"tileset.png", 25, 23, 16, 16);
-	}
+    public static void Load() {
+        _groundImages = Program.LoadSprites(@"tileset.png", 25, 23, 16, 16);
+    }
 
-	public static void Draw() {
-		for (int i = 0; i < TILE_COUNT; i++) {
-			Program.DrawEXGraph(i * Game.GROUND_SIZE, Game.GROUND_POS, Game.GROUND_SIZE, Game.GROUND_SIZE, _groundImages[28]);
-		}
-	}
+    public static void Draw() {
+        for (int i = 0; i < TILE_COUNT; i++) {
+            Program.DrawEXGraph(i * Game.GROUND_SIZE, Game.GROUND_POS, Game.GROUND_SIZE, Game.GROUND_SIZE, _groundImages[28]);
+        }
+    }
 }
--- Code/Program.cs
+++ Code/Program.cs
@@ -1,61 +1,59 @@
 ﻿using DxLibDLL;
 
-internal static class Game
-{
-	public static readonly int GROUND_SIZE = 32;
-	public static readonly int GROUND_POS = Program.SCREEN_Y - GROUND_SIZE;
+internal static class Game {
+    public static readonly int GROUND_SIZE = 32;
+    public static readonly int GROUND_POS = Program.SCREEN_Y - GROUND_SIZE;
 
-	public static void Run() {
-		Load();
-		Loop();
-	}
+    public static void Run() {
+        Load();
+        Loop();
+    }
 
-	private static void Load() {
-		Ground.Load();
-	}
+    private static void Load() {
+        Ground.Load();
+    }
 
-	private static void Loop() {
-		while (DX.ProcessMessage() == 0) {
-			Draw();
-		}
-	}
+    private static void Loop() {
+        while (DX.ProcessMessage() == 0) {
+            Draw();
+        }
+    }
 
-	private static void Draw() {
-		DX.ClearDrawScreen();
+    private static void Draw() {
+        DX.ClearDrawScreen();
 
-		Ground.Draw();
+        Ground.Draw();
 
-		DX.ScreenFlip();
-	}
+        DX.ScreenFlip();
+    }
 }
 
-internal static class Program
-{
-	public static readonly int SCREEN_X = 640, SCREEN_Y = 480;
+internal static class Program {
+    public static readonly int SCREEN_X = 640, SCREEN_Y = 480;
 
-	public static string ASSET_PATH = @"..\..\Assets\";
+    public static string ASSET_PATH = @"..\..\Assets\";
 
-	private static void Main() {
-		Init();
+    private static void Main() {
+        Init();
 
-		Game.Run();
+        Game.Run();
 
-		DX.DxLib_End();
-	}
+        DX.DxLib_End();
+    }
 
-	private static void Init() {
-		DX.ChangeWindowMode(DX.TRUE);
-		DX.DxLib_Init();
-		DX.SetDrawScreen(DX.DX_SCREEN_BACK);
-	}
+    private static void Init() {
+        DX.ChangeWindowMode(DX.TRUE);
+        DX.DxLib_Init();
+        DX.SetDrawScreen(DX.DX_SCREEN_BACK);
+    }
 
-	public static int[] LoadSprites(string filePath, int divX, int divY, int sizeX, int sizeY) {
-		int spriteCount = divX * divY;
-		int[] sprites = new int[spriteCount];
-		DX.LoadDivGraph(ASSET_PATH + filePath, spriteCount, divX, divY, sizeX, sizeY, sprites);
-		return sprites;
-	}
+    public static int[] LoadSprites(string filePath, int divX, int divY, int sizeX, int sizeY) {
+        int spriteCount = divX * divY;
+        int[] sprites = new int[spriteCount];
+        DX.LoadDivGraph(ASSET_PATH + filePath, spriteCount, divX, divY, sizeX, sizeY, sprites);
+        return sprites;
+    }
 
-	public static void DrawEXGraph(int posX, int posY, int sizeX, int sizeY, int GrHandle) =>
-		DX.DrawExtendGraph(posX, posY, posX + sizeX, posY + sizeY, GrHandle, DX.TRUE);
+    public static void DrawEXGraph(int posX, int posY, int sizeX, int sizeY, int GrHandle) =>
+        DX.DrawExtendGraph(posX, posY, posX + sizeX, posY + sizeY, GrHandle, DX.TRUE);
 }
